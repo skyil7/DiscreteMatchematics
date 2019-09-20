fdd89d18765609f2c4d5c678c3423d1953de7830
skyil7 <giopaik@naver.com>
2019-09-20 16:13:50 +0900

    문제 수정

--- 1주차/파이썬 실력 테스트/2.티켓구매.py
+++ 1주차/파이썬 실력 테스트/2.티켓구매.py
@@ -5,6 +5,9 @@ rank = int(input("탑승 클래스 : "))
 cost = [0,2000000,1000000,500000, 0]
 discount = [0, 0.7, 0.8, 0.9, 0]
 
-budget -= cost[rank]*discount[rank]
+if coupon:
+    budget -= cost[rank]*discount[rank]
+else:
+    budget -= cost[rank]
 
 print("남은 돈 :",int(budget))
