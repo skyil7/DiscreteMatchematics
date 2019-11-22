b26faaba818e9196cc3aa81e6ad5d0bb2b93b88d
skyil7 <giopaik@naver.com>
2019-11-22 16:02:55 +0900

    정리

--- 1주차/파이썬 실력 테스트/1.여행경비마련.py
+++
@@ -1,8 +0,0 @@
-stock = int(input("주식 펀드 투자 금액 : "))
-bank = int(input("정기 예금 저축 금액 : "))
-stockFloat = (float(input("주식 펀드 이자율 : "))+100)/100
-bankFloat = (float(input("정기 예금 이자율 : "))+100)/100
-
-stock, bank = bank, stock
-
-print("총 수익 :",int(bank*bankFloat + stock*stockFloat))
